4ae816c954f78676c9378a5d9f196f938de7152b
Adam Jones <jones_adam@rocketmail.com>
2017-12-11 16:02:03 +0000

    Update index.js to use i18n package

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,24 @@ const ApiAiApp = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
 const https = require('https');
 const dashbot = require('dashbot')('fC1GphoDWue8172wjcpSEFv1MXF4h7aemj3hxGBn').google;
+const i18n = require('i18n');
+
+i18n.configure({
+  locales: ['en'],
+  directory: __dirname + '/locales',
+  defaultLocale: 'en'
+});
 
 exports.chargePointr = functions.https.onRequest((request, response) => {
 	const app = new ApiAiApp({request: request, response: response});
 	dashbot.configHandler(app);
+	i18n.setLocale(app.getUserLocale());
 
     function askLocationPermission (app) {
 		app.setContext('Nearestchargers-followup');
 		app.setContext('charger-levels', 1, {levels: app.getArgument('chargerLevels')});
 
-        app.askForPermission('To find the nearest chargers', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
+        app.askForPermission(i18n.__('PERMISSION_REASON'), app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
     }
 
     function findChargersWithLocation (app) {
@@ -36,7 +44,7 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 					if(charger.AddressInfo && charger.Connections[0]) {
 						options.push({
 							title: charger.AddressInfo.Title,
-							description: (JSON.stringify(chargerLevels) == "[\"3\"]" ? 'Level 3: High (Over 40kW)' : charger.Connections[0].Level.Title) + ', '
+							description: (getMaxLevelTitle(charger.Connections)) + ', '
 										+ (charger.Connections[0].CurrentType ? charger.Connections[0].CurrentType.Title + ', '  : '')
 										+ (charger.UsageType.Title.substr(9) ? charger.UsageType.Title.substr(9) + ', ' : '')
 										+ charger.AddressInfo.Distance.toString().substr(0, 3) + 'mi',
@@ -46,8 +54,8 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 					}
                 });
 
-                let speech = 'Which of these chargers sounds good?';
-                let title = 'Nearby chargers';
+                let speech = i18n.__('WHICH_CHARGER');
+                let title = i18n.__('NEARBY_CHARGERS');
 
 				if (!app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
 					speech += ' ';
@@ -59,7 +67,7 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
                 askWithList(speech, title, options);
             });
         } else {
-            askWithLinkAndSuggestions('Unfortunately I can\'t find charge points near you without your location. Do you want to try again?', 'Open Charge Map', 'http://openchargemap.org/app/', ['Yes', 'No']);
+            askWithLinkAndSuggestions(i18n.__('PERMISSION_FAILED'), 'Open Charge Map', 'https://openchargemap.org/app/', [i18n.__('YES'), i18n.__('NO')]);
         }
     }
 
@@ -69,33 +77,33 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 		getJSON(url, function (data) {
 			let charger = data[0];
 
-			let speech = 'Here are the details for that charger. Do you want directions or attributions data for it?';
+			let speech = i18n.__('CHARGER_HERE_ARE_DETAILS') + ' ' + i18n.__('DIRECTIONS_OR_ATTRIBUTIONS');
 			if (!app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-				speech = '<speak>That charger is ' + charger.StatusType.Title + ', and has ';
-				speech += (charger.Connections[0].Quantity ? charger.Connections[0].Quantity + ' ' : 'a '); // 1
-				speech += (charger.Connections[0].Level ? charger.Connections[0].Level.Title + ' ' : ''); // Level 2 : Medium (Over 2kW)
+				speech = '<speak>' + i18n.__('THAT_CHARGER_IS') + ' ' + charger.StatusType.Title + ', ' + i18n.__('AND_HAS') + ' ';
+				speech += (charger.Connections[0].Quantity ? charger.Connections[0].Quantity + ' ' : '1 '); // 1
+				speech += (charger.Connections[0].LevelID ? getLevelTitle(charger.Connections[0].LevelID) + ' ' : ''); // Level 2 : Medium (Over 2kW)
 				speech += (charger.Connections[0].ConnectionType ? charger.Connections[0].ConnectionType.Title + ' ' : ''); // Mennekes (Type 2)
 				speech += (charger.Connections[0].CurrentType ? charger.Connections[0].CurrentType.Title + ' ' : ''); // AC (Single-Phase)
-				speech += (charger.Connections[0].Quantity > 1 ? 'connections. ' : 'connection. ');
-				speech += 'It\'s located at ' + charger.AddressInfo.AddressLine1 + (charger.AddressInfo.Postcode ? ', <say-as interpret-as="digits">' + charger.AddressInfo.Postcode : '') + '</say-as>.';
-				speech += 'Do you want to exit or get attributions info for it?</speak>';
+				speech += (charger.Connections[0].Quantity > 1 ? i18n.__('CONNECTIONS').toLowerCase() + '. ' : i18n.__('CONNECTION').toLowerCase() + '. ');
+				speech += i18n.__('ITS_LOCATED_AT') + ' ' + charger.AddressInfo.AddressLine1 + (charger.AddressInfo.Postcode ? ', <say-as interpret-as="digits">' + charger.AddressInfo.Postcode : '') + '</say-as>.';
+				speech += i18n.__('EXIT_OR_ATTRIBUTIONS') + '</speak>';
 			}
 
 			let title = charger.AddressInfo.Title;
-			let destinationName = 'View on Open Charge Map';
+			let destinationName = i18n.__('VIEW_ON_OCM');
 			let suggestionUrl = 'https://openchargemap.org/site/poi/details/' + app.getSelectedOption();
 
 			let text = '';
 
-			text += (charger.StatusType.Title ? '**Status:** ' + charger.StatusType.Title + '  \n' : '');
-			text += (charger.AddressInfo.AccessComments ? '**Acess:** ' + charger.AddressInfo.AccessComments + '  \n' : '');
+			text += (charger.StatusType.Title ? '**' + i18n.__('STATUS') + ':** ' + charger.StatusType.Title + '  \n' : '');
+			text += (charger.AddressInfo.AccessComments ? '**' + i18n.__('ACCESS') + ':** ' + charger.AddressInfo.AccessComments + '  \n' : '');
 
-			text += '**Address:**  \n';
+			text += '**' + i18n.__('ADDRESS') + ':**  \n';
 			text += charger.AddressInfo.AddressLine1 + '  \n';
 			text += (charger.AddressInfo.AddressLine2 ? charger.AddressInfo.AddressLine2 + '  \n': '');
 			text += (charger.AddressInfo.Postcode ? charger.AddressInfo.Postcode + '  \n' : '');
 
-			text += '**Connections:**  \n';
+			text += '**' + i18n.__('CONNECTIONS') + ':**  \n';
 			charger.Connections.forEach(function (connection) {
 				text += (connection.Quantity ? connection.Quantity + 'x ' : '1x '); // 1
 				text += (connection.Level ? connection.Level.Title + ' ' : ''); // Level 2 : Medium (Over 2kW)
@@ -104,11 +112,11 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 				text += '  \n';
 			});
 
-			text += (charger.UsageCost ? '**Cost:** ' + charger.UsageCost + '  \n': '');
-			text += (charger.DataProvider ? '**Source:** ' + charger.DataProvider.Title : '');
+			text += (charger.UsageCost ? '**' + i18n.__('COST') + ':** ' + charger.UsageCost + '  \n': '');
+			text += (charger.DataProvider ? '**' + i18n.__('SOURCE') + ':** ' + charger.DataProvider.Title : '');
 
 			app.setContext('charger', 3, charger);
-			askWithBasicCardAndLinkAndSuggestions(speech, title, text, destinationName, suggestionUrl, ['Directions', 'Attributions', 'Search again', 'No thanks, bye']);
+			askWithBasicCardAndLinkAndSuggestions(speech, title, text, destinationName, suggestionUrl, [i18n.__('DIRECTIONS'), i18n.__('SOURCE'), i18n.__('SEARCH_AGAIN'), i18n.__('EXIT')]);
 		});
     }
 
@@ -119,12 +127,12 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 				let suggestionUrl = 'https://maps.google.com?saddr=Current+Location&daddr=' + charger.AddressInfo.Latitude.toString() + ',' + charger.AddressInfo.Longitude.toString();
 
 				app.setContext('charger', 3, charger);
-				askWithLinkAndSuggestions('Sure, open the link for directions on Google Maps. Alternatively, what else would you like to do?', 'Google Maps', suggestionUrl, ['Search again', 'Exit']);
+				askWithLinkAndSuggestions(i18n.__('OPEN_GOOGLE_MAPS'), 'Google Maps', suggestionUrl, [i18n.__('SEARCH_AGAIN'), i18n.__('EXIT')]);
 			} else {
-				askSimpleResponseWithSuggestions('Sorry, you need to search for chargers first. Should I do that now?', ['Yeah', 'No thanks, bye']);
+				askSimpleResponseWithSuggestions(i18n.__('SEARCH_CHARGERS_FIRST'), [i18n.__('YES'), i18n.__('NO')]);
 			}
 		} else {
-			askSimpleResponse('Unfortunately directions aren\'t supported on your device. What else would you like to do?');
+			app.ask(i18n.__('DIRECTIONS_NOT_SUPPORTED'));
 		}
 	}
 
@@ -136,14 +144,14 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 			if(charger.DataProvider) {
 				let destinationName = charger.DataProvider.Title;
 				let suggestionUrl = charger.DataProvider.WebsiteURL;
-				let speech = 'Data by ' + charger.DataProvider.Title + ', ' + charger.DataProvider.License + '. What else can I help you with?';
+				let speech = charger.DataProvider.Title + ', ' + charger.DataProvider.License + '. ' + i18n.__('WHAT_ELSE_HELP');
 
-				askWithLinkAndSuggestions(speech, destinationName, suggestionUrl, ['Find other chargers', 'Get directions', 'Nothing, bye']);
+				askWithLinkAndSuggestions(speech, destinationName, suggestionUrl, [i18n.__('SEARCH_AGAIN'), i18n.__('DIRECTIONS'), i18n.__('EXIT')]);
 			} else {
-				askSimpleResponseWithSuggestions('Data provider information is missing for that charger. Do you want to search again?', ['Find other chargers', 'Thanks, bye']);
+				askSimpleResponseWithSuggestions(i18n.__('DATA_MISSING') + ' ' + i18n.__('WHAT_ELSE_HELP'), [i18n.__('SEARCH_AGAIN'), i18n.__('DIRECTIONS'), i18n.__('EXIT')]);
 			}
 		} else {
-			askSimpleResponseWithSuggestions('Sorry, you need to search for chargers first. Do you want me to do that now?', ['Yes', 'No, exit']);
+			askSimpleResponseWithSuggestions(i18n.__('SEARCH_CHARGERS_FIRST'), [i18n.__('YES'), i18n.__('NO')]);
 		}
 	}
 
@@ -200,7 +208,7 @@ exports.chargePointr = functions.https.onRequest((request, response) => {
 });
 
 function getJSON(url, callback) {
-    let req = https.get(url, function(res) {
+    https.get(url, function(res) {
         let data = '';
 
         res.on('data', function(chunk) {
@@ -213,6 +221,21 @@ function getJSON(url, callback) {
     });
 }
 
-function randomFromArray(arr) {
-	return arr[Math.floor(Math.random() * arr.length)];
+function getMaxLevelTitle(connections) {
+	let maxLevel = 1;
+
+	for (var i = 0; i < connections.length; i++) {
+		maxLevel = Math.max(maxLevel, connections.LevelID);
+	}
+
+	return getLevelTitle(maxLevel);
+}
+
+function getLevelTitle(level_number) {
+	switch(level_number) {
+		case 1: return i18n.__('LEVEL_1');
+		case 2: return i18n.__('LEVEL_2');
+		case 3: return i18n.__('LEVEL_3');
+		default: return '';
+	}
 }
